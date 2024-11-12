e9943ee7d33488a51d918ef33d6a828e3c485b9f
nicoleolivera <nicoleolivera08@hotmail.com>
2024-11-12 19:19:20 -0300

    Commit inicial home page, img y css

---
+++ js/personalizacion.js
@@ -0,0 +1,13 @@
+// botón que cambia el color de la web según el personaje clickeado
+
+let btnMario
+let btnBowser
+let btnPeach
+let btnLuigi
+
+btnMario.addEventListener ('click', function(e) {
+e.preventDefault();
+
+
+    
+});
