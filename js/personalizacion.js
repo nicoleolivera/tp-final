a2798d0a69752b16e4f1872af714c369ae2240e1
nicoleolivera <nicoleolivera08@hotmail.com>
2024-11-16 16:12:16 -0300

    Contenido de texto, imágen y video, y js

--- js/personalizacion.js
+++ js/personalizacion.js
@@ -1,13 +1,31 @@
 // botón que cambia el color de la web según el personaje clickeado
 
-let btnMario
-let btnBowser
-let btnPeach
-let btnLuigi
+let imgMario = document.querySelector('#imgMario');
+let imgBowser = document.querySelector('#imgBowser');
+let imgPeach = document.querySelector('#imgPeach');
+let imgLuigi = document.querySelector('#imgLuigi');
 
-btnMario.addEventListener ('click', function(e) {
-e.preventDefault();
-
-
-    
-});
+imgMario.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.body.style.backgroundColor = 'red';
+    document.body.style.color = 'white';
+  });
+  
+  imgBowser.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'yellow';
+  });
+  
+  imgPeach.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.body.style.backgroundColor = 'pink';
+    document.body.style.color = 'white';
+  });
+  
+  imgLuigi.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.body.style.backgroundColor = 'green';
+    document.body.style.color = 'white';
+  });
+  
