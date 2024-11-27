4d20ea8634847dce959dc9d9ca1cc4a1d361c197
nicoleolivera <nicoleolivera08@hotmail.com>
2024-11-27 10:39:52 -0300

    Primeros avances en el juego

--- juego/js/juego.js
+++ juego/js/juego.js
@@ -1,32 +1,86 @@
-//Cada vez que se hace click en el boton se muestra una pregunta al azar
-
+// Referencias a los elementos en el DOM
 let mostrarPregunta = document.querySelector('#contenedorPregunta');
 let btnPreguntame = document.querySelector('#btnPreguntame');
+let btnReiniciar = document.querySelector('#btnReiniciar');
+let mensajeResultado = document.querySelector('#mensajeResultado');
+let radios = document.querySelectorAll('.respuesta');
 
-let cantPreguntas = 0; // Inicializamos el contador
+let puntaje = 0; // Inicializar puntaje
+let cantPreguntas = 0; // Contador de preguntas
 
+// Preguntas divididas en verdaderas y falsas
+let preguntasVerdaderas = [
+    
+];
+
+let preguntasFalsas = [
+    
+];
+
+let preguntaActual = ''; // Almacena el texto de la pregunta que se mostrará
+let esVerdadera = false; // Determina si la pregunta es verdadera o falsa
+
+// Evento para mostrar una nueva pregunta
 btnPreguntame.addEventListener('click', function () {
-    let preguntas = [
-       
-    ];
+    let aleatorio = Math.floor(Math.random() * 6) + 1; // 1, 2, 3, 4, 5, 6
+    let elegida;
 
-    // Mostrar una pregunta aleatoria
-    let preguntaAzar = Math.floor(Math.random() * preguntas.length);
-    mostrarPregunta.innerText = preguntas[preguntaAzar];
+    if (aleatorio >= 1 && aleatorio <= 3) {
+        elegida = Math.floor(Math.random() * preguntasVerdaderas.length);
+        preguntaActual = preguntasVerdaderas[elegida];
+        esVerdadera = true;
+    } else {
+        elegida = Math.floor(Math.random() * preguntasFalsas.length);
+        preguntaActual = preguntasFalsas[elegida];
+        esVerdadera = false;
+    }
 
-    // Incrementar el contador de preguntas
+    mostrarPregunta.innerText = preguntaActual;
+    mensajeResultado.innerText = ''; // Limpiar mensaje previo
     cantPreguntas++;
 
-    // Si llegamos a 5, deshabilitamos el botón
-    if (cantPreguntas === 5) {
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].checked = false;
+    }
+    
+
+    if (cantPreguntas === 11) {
         btnPreguntame.disabled = true;
         mostrarPregunta.innerText = '¡Se acabaron las preguntas!';
     }
 });
 
-// mostrar puntaje Y para eso ir sumando los checkkkradios
+// Evento para validar la respuesta de los radio buttons
+radios.forEach(radio => {
+    radio.addEventListener('change', function () {
+        if (preguntaActual === '') {
+            mensajeResultado.innerText = 'Por favor, presiona "Pregúntame" primero.';
+            return;
+        }
 
-// btn para reiniciar juego si sequiere jugar otra vez o perdio 
+        let valorSeleccionado = radio.value;
 
+        if ((esVerdadera && valorSeleccionado === "true") || (!esVerdadera && valorSeleccionado === "false")) {
+            puntaje++;
+            mensajeResultado.innerText = '¡Correcto! Puntaje: ' + puntaje;
+        } else {
+            mensajeResultado.innerText = 'Incorrecto. Puntaje: ' + puntaje;
+        }
+    });
+});
 
+// Evento para reiniciar el juego
+btnReiniciar.addEventListener('click', function () {
+    puntaje = 0;
+    cantPreguntas = 0;
+    preguntaActual = '';
+    esVerdadera = false;
 
+    mostrarPregunta.innerText = 'Presiona el botón de preguntas para intentarlo de nuevo';
+    mensajeResultado.innerText = '';
+    btnPreguntame.disabled = false;
+
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].checked = false;
+    }
+});
