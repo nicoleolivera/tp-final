0cc8af0306b31eaf0b5f84bf15a3a47bc0decd81
nicoleolivera <nicoleolivera08@hotmail.com>
2025-02-08 21:30:26 -0300

    Juego - decremento de vidas

--- juego/js/juego.js
+++ juego/js/juego.js
@@ -1,4 +1,3 @@
-// numeros
 let numeroGanador = 1985;
 let numeroIngresado;
 
@@ -6,12 +5,14 @@ let numeroIngresado;
 let enviarRespuesta = document.querySelector('#enviar');
 let reiniciar;
 
-// mensajes // aun no sé si msjs inner.text o img
+// mensajes
 let mensajeBowser = document.querySelector('#mensajeBowser');
-let mensajeResultado; // ganador o perdedor
+let mensajeResultado = document.querySelector('#mensajeResultado'); // Elemento para mostrar si ganó o perdió
 
 // contador vidas/intentos
 let vidas = 5;
+let vidasTexto = document.querySelector('#vidas'); 
+
 
 // Validación del número ingresado
 enviarRespuesta.addEventListener('click', function(e) {
@@ -26,17 +27,32 @@ enviarRespuesta.addEventListener('click', function(e) {
     }
 });
 
-// Función de pistas
 function pistas() {
     if (numeroIngresado === numeroGanador) {
         mensajeBowser.innerText = '¡Correcto, es el año en el que nací! ... bueno, también el año en el que se lanzó Super Mario Bros. ¿Cómo lo supiste?';
-    } else if (numeroIngresado >= 1990) {
-        mensajeBowser.innerText = 'Estás yendo demasiado lejos, intenta bajar un poco.';
-    } else if (numeroIngresado >= 1986) {
-        mensajeBowser.innerText = 'Te pasaste por poco.';
-    } else if (numeroIngresado >= 1981) {
-        mensajeBowser.innerText = 'Estás cerca, pero no.';
+        mensajeResultado.innerText = '¡Ganaste!';
+        enviarRespuesta.disabled = true;
     } else {
-        mensajeBowser.innerText = 'Demasiado lejos, intenta subir.';
+        vidas--; // Restar una vida
+        vidasTexto.innerText = vidas; // Actualizar el contador en pantalla
+
+        if (vidas === 0) {
+            mensajeBowser.innerText = '¡Oh no! Te quedaste sin intentos.';
+            mensajeResultado.innerText = 'Perdiste. Inténtalo de nuevo.';
+            enviarRespuesta.disabled = true;
+            return;
+        }
+
+        // Dar pistas según el número ingresado
+        if (numeroIngresado >= 1990) {
+            mensajeBowser.innerText = 'Estás yendo demasiado lejos, intenta bajar un poco.';
+        } else if (numeroIngresado >= 1986) {
+            mensajeBowser.innerText = 'Te pasaste por poco.';
+        } else if (numeroIngresado >= 1981) {
+            mensajeBowser.innerText = 'Estás cerca, pero no.';
+        } else {
+            mensajeBowser.innerText = 'Demasiado lejos, intenta subir.';
+        }
     }
 }
+
