cdf776ec97ac49cbbdfbaa8bf2dca988e50105ef
nicoleolivera <nicoleolivera08@hotmail.com>
2025-02-12 13:08:07 -0300

    Modificaciones de texto y orden

--- juego/js/juego.js
+++ juego/js/juego.js
@@ -1,20 +1,20 @@
-// números
+// Variables con números para comparar
 let numeroGanador = 1985;
 let numeroIngresado;
 
-// contenedores
+// Contenedor
 let contenedorOculto = document.querySelector('#contenedorOculto');
 
-// botones
+// Botones
 let enviarRespuesta = document.querySelector('#enviar');
-let reiniciar = document.querySelector('#reiniciar'); // Botón de reinicio
+let reiniciar = document.querySelector('#reiniciar');
 let continuar = document.querySelector('#continuar');
 
-// mensajes
+// Mensajes
 let mensajeBowser = document.querySelector('#mensajeBowser');
-let mensajeResultado = document.querySelector('#mensajeResultado'); // Elemento para mostrar si ganó o perdió
+let mensajeResultado = document.querySelector('#mensajeResultado');
 
-// contador vidas/intentos
+// Contadores y texto de vidas e intentos
 let vidas = 5;
 let vidasTexto = document.querySelector('#vidas');
 let intentos = 3;
@@ -24,43 +24,44 @@ let intentosTexto = document.querySelector('#intentos');
 // Validación del número ingresado
 enviarRespuesta.addEventListener('click', function(e) {
     e.preventDefault();
-    numeroIngresado = parseInt(document.querySelector('#respuesta').value); // Convertir a número
+    numeroIngresado = Number(document.querySelector('#respuesta').value);
 
     if (isNaN(numeroIngresado)) {
         alert('El casillero no puede estar vacío o no es un número');
     } else {
         console.log('Número ingresado:', numeroIngresado);
         pistas();
-        document.querySelector('#respuesta').value = ''; // Limpiar el campo de respuesta después de enviar
+        document.querySelector('#respuesta').value = '';
     }
 });
 
-
+// Función para orientar la respuesta del usuario con pistas
 function pistas() {
+
+    //En caso de que acierte el número - gana
     if (numeroIngresado === numeroGanador) {
-        mensajeBowser.innerText = '¡Correcto, es el año en el que nací! ... bueno, también el año en el que se lanzó Super Mario Bros. ¿Cómo lo supiste?';
+        mensajeBowser.innerText = '¡Correcto! Es el año en el que nací... bueno, también el año en que se lanzó Super Mario Bros. ¿Cómo lo supiste?';
         mensajeResultado.innerText = '¡Ganaste!';
         enviarRespuesta.disabled = true;
         continuar.style.display = 'block';
     } else {
-        vidas--; // Restar una vida
-        vidasTexto.innerText = `❤️ Vidas: ${vidas}`; // Actualizar el contador en pantalla
+        vidas--;
+        vidasTexto.innerText = `❤️ Vidas: ${vidas}`;
 
+        //En caso de que se quede sin vidas - pierde un intento
         if (vidas === 0) {
-            mensajeBowser.innerText = '¡Te quedaste sin vidas! 😝 Ahora estoy dudando de si realmente recorriste la página, pero está bien... te doy una última pista: si sigues navegando no muy lejos de aquí, encontrarás un año muy importante para la franquicia. Intenta aprender algo de historia.';
+            mensajeBowser.innerText = '¡Te quedaste sin vidas! 😝 Ahora estoy dudando de si realmente recorriste la página, pero está bien... te doy una última pista: Si sigues navegando, no muy lejos de aquí encontrarás un año muy importante para la franquicia. Intenta aprender algo de historia.';
             mensajeResultado.innerText = 'Perdiste. Inténtalo de nuevo.';
             enviarRespuesta.disabled = true;
-
-            // Mostrar el botón de reinicio
             reiniciar.style.display = 'block';
             return;
         }
 
         // Dar pistas según el número ingresado
         if (numeroIngresado >= 1990) {
-            mensajeBowser.innerText = 'Estás yendo demasiado lejos, intenta bajar un poco. Creo recordar que fue antes de los 90s 🤔';
+            mensajeBowser.innerText = 'Estás yendo demasiado lejos, intenta bajar un poco. Creo recordar que fue antes de los 90 🤔';
         } else if (numeroIngresado == 1989) {
-            mensajeBowser.innerText = 'Esta fecha no es, es lo único que quiero decir 😒';
+            mensajeBowser.innerText = 'Esta no es la fecha, es lo único que quiero decir 😒';
         } else if (numeroIngresado == 1988) {
             mensajeBowser.innerText = 'Estás lamentablemente cerca, pero te pasaste.';
         } else if (numeroIngresado == 1987) {
@@ -70,19 +71,19 @@ function pistas() {
         } else if (numeroIngresado == 1984) {
             mensajeBowser.innerText = 'Te encuentras realmente cerca, ¿por qué no mejor abandonas el juego? 🙂‍↕️';
         } else if (numeroIngresado == 1983) {
-            mensajeBowser.innerText = 'Creo que te estás confundiendo de juego... Sí, Donkey Kong se lanzó en este año, pero no estamos hablando de él.';
+            mensajeBowser.innerText = 'Creo que te estás confundiendo de juego... Sí, Donkey Kong se lanzó este año, pero no estamos hablando de él.';
         } else if (numeroIngresado == 1982) {
             mensajeBowser.innerText = 'Casi, pero yo intentaría un par de números más arriba.';
         } else if (numeroIngresado == 1981) {
             mensajeBowser.innerText = 'Estás cerca, pero no.';
         } else if (numeroIngresado >= 1970 && numeroIngresado < 1980) {
-            mensajeBowser.innerText = 'Sí... bueno, en los 70s se crearon las primeras consolas de videojuegos 👾 pero aún yo no existía.';
+            mensajeBowser.innerText = 'Sí... bueno, en los 70 se crearon las primeras consolas de videojuegos 👾, pero yo aún no existía.';
         } else if (numeroIngresado >= 1950 && numeroIngresado < 1970) {
-            mensajeBowser.innerText = 'Bueno, quizás durante los 50s y los 60s podamos hablar de computadoras, pero estoy intentando hablar de mí y no hay Bowser entre estas fechas.';
+            mensajeBowser.innerText = 'Bueno, quizás durante los 50 y los 60 podamos hablar de computadoras, pero estoy intentando hablar de mí y no hay Bowser entre estas fechas.';
         } else if (numeroIngresado >= 1900 && numeroIngresado < 1940) {
-            mensajeBowser.innerText = '¿Qué edad creés que tengo? antes de los 40s ni existían los píxeles 😒';
+            mensajeBowser.innerText = '¿Qué edad crees que tengo? antes de los 40 ni existían los píxeles 😒';
         } else {
-            mensajeBowser.innerText = 'No creo que esa fecha tenga sentido. ¿Recuerdas que estamos hablando de mí? Bueno, y también de Mario. Yo al menos intentaría con alguna fecha dentro del siglo XX. 🙄😒';
+            mensajeBowser.innerText = 'No creo que esa fecha tenga sentido. Recuerda que estamos hablando de mí... bueno, y también de Mario. Yo al menos intentaría con alguna fecha dentro del siglo XX. 🙄😒';
         }
     }
 }
@@ -90,7 +91,7 @@ function pistas() {
 // Reiniciar el juego
 reiniciar.addEventListener('click', function() {
     if (intentos > 0) {
-        intentos--; // Restar un intento
+        intentos--;
         vidas = 5;
         vidasTexto.innerText = `❤️ Vidas: ${vidas}`;
         intentosTexto.innerText = `💀 Intentos: ${intentos}`;
@@ -101,15 +102,15 @@ reiniciar.addEventListener('click', function() {
         reiniciar.style.display = 'none';
     }
 
+    // Fin del juego sin ganar
     if (intentos === 0) {
-        mensajeBowser.innerText = 'Se acabaron tus intentos. No puedes jugar más.';
+        mensajeBowser.innerText = 'Se acabaron tus intentos. No puedes jugar más. 🤭';
         reiniciar.disabled = true;
         enviarRespuesta.disabled = true;
     }
 });
 
-
-
+// Fin del juego ganando - accede a la sección del sitio oculta
 continuar.addEventListener('click', function(e) {
     e.preventDefault();
 
