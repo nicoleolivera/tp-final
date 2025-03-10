cc9599cc628852c44ed23868f07f2f871bb4f63f
nicoleolivera <nicoleolivera08@hotmail.com>
2025-03-10 09:45:09 -0300

    Cambio en el nombre de las funciones

--- personajes/js/personalizacion.js
+++ personajes/js/personalizacion.js
@@ -18,12 +18,12 @@ imgMario.addEventListener('click', function (e) {
   e.preventDefault();
   document.body.style.backgroundColor = '#0091ff';
 
-  document.querySelectorAll('header, main, footer').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#bf0006';
+  document.querySelectorAll('header, main, footer').forEach(function (seccionesPrincipalesMario) {
+    seccionesPrincipalesMario.style.backgroundColor = '#bf0006';
   });
 
-  document.querySelectorAll('aside, section').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#1136bd';
+  document.querySelectorAll('aside, section').forEach(function (seccionesSecundariasMario) {
+    seccionesSecundariasMario.style.backgroundColor = '#1136bd';
   });
 });
 
@@ -32,12 +32,12 @@ imgLuigi.addEventListener('click', function (e) {
   e.preventDefault();
   document.body.style.backgroundColor = '#06bf00';
 
-  document.querySelectorAll('header, main, footer').forEach(function (secciones) {
-   secciones.style.backgroundColor = '#55c425';
+  document.querySelectorAll('header, main, footer').forEach(function (seccionesPrincipalesLuigi) {
+    seccionesPrincipalesLuigi.style.backgroundColor = '#55c425';
  });
 
-  document.querySelectorAll('aside, section').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#90e36c';
+  document.querySelectorAll('aside, section').forEach(function (seccionesSecundariasLuigi) {
+    seccionesSecundariasLuigi.style.backgroundColor = '#90e36c';
  });
 });
 
@@ -46,12 +46,12 @@ imgPeach.addEventListener('click', function (e) {
   e.preventDefault();
   document.body.style.backgroundColor = '#ffa3d0';
   
-  document.querySelectorAll('header, main, footer').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#25a5c4';
+  document.querySelectorAll('header, main, footer').forEach(function (seccionesPrincipalesPeach) {
+    seccionesPrincipalesPeach.style.backgroundColor = '#25a5c4';
   });
  
-  document.querySelectorAll('aside, section').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#c42555';
+  document.querySelectorAll('aside, section').forEach(function (seccionesSecundariasPeach) {
+    seccionesSecundariasPeach.style.backgroundColor = '#c42555';
   });
 });
 
@@ -60,12 +60,12 @@ imgBowser.addEventListener('click', function (e) {
   e.preventDefault();
   document.body.style.backgroundColor = '#000000';
 
-  document.querySelectorAll('header, main, footer').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#fff700';
+  document.querySelectorAll('header, main, footer').forEach(function (seccionesPrincipalesBowser) {
+    seccionesPrincipalesBowser.style.backgroundColor = '#fff700';
   });
    
-  document.querySelectorAll('aside, section').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#ffa200';
+  document.querySelectorAll('aside, section').forEach(function (seccionesSecundariasBowser) {
+    seccionesSecundariasBowser.style.backgroundColor = '#ffa200';
   });
 });
   
@@ -74,12 +74,12 @@ imgToad.addEventListener('click', function (e) {
   e.preventDefault();
   document.body.style.backgroundColor = '#ff0000';
 
-  document.querySelectorAll('header, main, footer').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#ffffff';
+  document.querySelectorAll('header, main, footer').forEach(function (seccionesPrincipalesToad) {
+    seccionesPrincipalesToad.style.backgroundColor = '#ffffff';
   });
    
-  document.querySelectorAll('aside, section').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#0000d6';
+  document.querySelectorAll('aside, section').forEach(function (seccionesSecundariasToad) {
+    seccionesSecundariasToad.style.backgroundColor = '#0000d6';
   });
 });
 
@@ -87,15 +87,15 @@ imgToad.addEventListener('click', function (e) {
 imgYoshi.addEventListener('click', function (e) {
   e.preventDefault();
   document.body.style.backgroundColor = '#cb0b0b';
-  document.body.style.color = '#000000';
+  document.body.style.color = '#000000'; // letras
 
-  document.querySelectorAll('header, main, footer').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#55c425';
-    secciones.style.color = '#000000';
+  document.querySelectorAll('header, main, footer').forEach(function (seccionesPrincipalesYoshi) {
+    seccionesPrincipalesYoshi.style.backgroundColor = '#55c425';
+    seccionesPrincipalesYoshi.style.color = '#000000';
   });
    
-  document.querySelectorAll('aside, section').forEach(function (secciones) {
-    secciones.style.backgroundColor = '#ffffff';
-    secciones.style.color = '#000000';
+  document.querySelectorAll('aside, section').forEach(function (seccionesSecundariasYoshi) {
+    seccionesSecundariasYoshi.style.backgroundColor = '#ffffff';
+    seccionesSecundariasYoshi.style.color = '#000000';
   });
 });
